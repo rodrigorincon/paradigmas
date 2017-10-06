b6076153e79bc0da6975e7a2a8ed5175dd523bff
rodrigo rincon <rodrigo.rincon@altran-ais.com.br>
2017-10-06 07:12:27 -0300

    pequenas alterações

--- desafio/desafio2/app/controllers/anexos_controller.rb
+++ desafio/desafio2/app/controllers/anexos_controller.rb
@@ -1,6 +1,6 @@
 class AnexosController < ApplicationController
 
-  def show
+  def get_file
     anexo = Anexo.find(params[:id])
     send_file 'documents/'+anexo.name ,:filename => anexo.name, :status => 200, layout: false, :type => anexo.myme_type
   end
--- desafio/desafio2/config/routes.rb
+++ desafio/desafio2/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
-  get 'anexos/:id', to: 'anexos#show'
+  get 'anexos/:id', to: 'anexos#get_file'
   patch 'tasks/:id/complete', to: 'tasks#complete'
 
   resources :sessions, only: [:create, :destroy]
